4b4475f6503078c9b848afd98bd68ad6c3d9935d
Jaime Torres <andrestbermejoj@gmail.com>
2023-01-28 12:59:16 -0500

    remove tests, add main

--- labs/Lab1/src/EjThreads01.java
+++ labs/Lab1/src/EjThreads01.java
@@ -2,4 +2,8 @@ public class EjThreads01 extends Thread {
     public void run() {
         System.out.println("Extendiendo Thread...");
     }
+    public static void main(String[] args) {
+        EjThreads01 t = new EjThreads01();
+        t.start();
+    }
 }
