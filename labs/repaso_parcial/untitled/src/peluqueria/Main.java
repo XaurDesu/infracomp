979595e32f505aa86d9086500b275fce66080873
Jaime Torres <andrestbermejoj@gmail.com>
2023-03-04 10:34:35 -0500

    add more

--- labs/repaso_parcial/untitled/src/peluqueria/Main.java
+++ labs/repaso_parcial/untitled/src/peluqueria/Main.java
@@ -1,7 +1,10 @@
 package peluqueria;
 
 public class Main {
+
     public static void main(String[] args) {
+        Peluqueria p = new Peluqueria(20);
+        
         System.out.println("Hello world!");
     }
 }
